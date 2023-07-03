6af8317353e4a29443f9d44fba42f07366e0babe
Singhnav13 <navjot131@gmail.com>
2023-07-03 14:35:22 +0530

    Input name added in Form.js

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,8 +1,24 @@
 import React, { useState } from 'react'
 
 const Form = () => {
+  const [name, setName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${name}`)
+  }
+
   return (
-    <div>Form</div>
+    <form onSubmit={handleSubmit}>
+      <label>Enter your name:
+        <input 
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <input type="submit" />
+    </form>
   )
 }
 

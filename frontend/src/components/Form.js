50cbb2ec4f275f950314039918eef8cb04fdeea0
Singhnav13 <navjot131@gmail.com>
2023-07-04 16:15:00 +0530

    using jsPDF library to convert form data to pdf

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import jsPDF from 'jspdf';
 
 const Form = () => {
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const doc = new jsPDF({
+    orientation: 'landscape',
+    unit: 'in',
+    format: [4, 2],
+  });
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    let name1 = {name};
-    html2pdf(name1);
+    doc.html(name, {
+      async callback(doc) {
+        await doc.save('pdf_name');
+      },
+    });
   }
 
   return (
@@ -16,6 +27,13 @@ const Form = () => {
           type="text" 
           value={name}
           onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <label>Enter your Email:
+        <input 
+          type="text" 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
       </label>
       <input type="submit" />
